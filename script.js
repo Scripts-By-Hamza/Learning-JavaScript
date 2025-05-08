39b3d32a7ecb28c058a25ccd9a7d26d3fe1c21a9
Muhammad Hamza <rajpoothamza2412@gmail.com>
2025-05-08 10:13:15 +0500

    Update script.js
    Add String Components

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ let number = 21
 //this is number data type
 
 let email = "hamza123@gamil.com"
+let fruit = "Apple, Bananan, Mango"
 //this is string data type
 
 let temp = null
@@ -39,5 +40,20 @@ console.log (email);
 console.log (temp);
 console.log (phoneNumber);
 console.log (math);
+
+//****************************************************
+// String
+// String has more components like lenght, trim, split etc 
+
+console.log(email.length);
+console.log(name.length);
+
+console.log(name.charAt(5));
+
+console.log(name.replace('Muhammad', 'Hamza'));
+console.log(name.replace('Hamza', 'Awais'));
+
+console.log(fruit.split(','));
+
 
 
