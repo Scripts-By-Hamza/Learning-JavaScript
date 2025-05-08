dc0b50a2d92df1a7887827b9a1aa56ec955c7a8a
Muhammad Hamza <rajpoothamza2412@gmail.com>
2025-05-08 09:31:39 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning-JavaScript
+Here i am saving all my practice code and notes of javascript
---
+++ script.js
@@ -0,0 +1,43 @@
+/* VARIABLES
+There are three variables used in javascript. 
+Const
+Let
+Var */
+
+const accountId = 348 //the value save in const can not be changed in future.
+
+console.log(accountId);
+
+let name = 'Muhammad Hamza' //the value save in let can be reassigned in future
+
+console.log(name);
+
+/* it is paraferable to not use var in code */
+
+
+//********************************************************
+
+//Data Types
+
+let number = 21
+//this is number data type
+
+let email = "hamza123@gamil.com"
+//this is string data type
+
+let temp = null
+//this is null data type
+
+let phoneNumber
+// this is undefined
+
+let math = (5>2)
+// this is boolean data type
+
+console.log (number);
+console.log (email);
+console.log (temp);
+console.log (phoneNumber);
+console.log (math);
+
+
